83541d9cda00f3aa01c0ab0c15fddcd29ab73ca6
Rkpadevops <140993100+Rkpadevops@users.noreply.github.com>
2023-08-02 22:54:55 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,23 @@
+class Addition
+{
+	public static void main(String args[])
+	{
+		int number1,number2,res;
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter num1 value");
+		number1=s.nextInt();
+		System.out.println("Enter num2 vlue");
+		number2=s.nextInt();
+		res=number1+number2;
+		System.out.println("The result is "+res);
+		res=numeber1*number2;
+		System.out.println("The result od product is : "+res);
+		System.out.println("This is first line");
+		System.out.println("This is second line");
+		System.out.println("This is third line");
+		System.out.println("This is fourth line");
+		System.out.println("This is fifth line");
+		System.out.println("This is sixth line");
+		System.out.println("This is seventh line");
+	}
+}
